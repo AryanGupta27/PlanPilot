e5e2ca27ed8da5581a1230540062feafb71f9c20
AryanGupta27 <rlyaryan.1827@gmail.com>
2023-09-14 18:14:47 +0530

    Adding all the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./Task";
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      // we created a new object
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completeStatus: false,
+    };
+
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    const newToDoList = todoList.filter((task) => {
+      if (task.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    setTodoList(newToDoList);
+
+    /*  orsimply 
+      return(
+        task.id !== taskName.id 
+      )   
+    */
+  };
+
+  const updateTask = (id) => {
+    const newUpdatedList = todoList.map((task) => {
+      if (task.id === id) {
+        return { ...task, completeStatus: true };
+      } else {
+        return task;
+      }
+    });
+    setTodoList(newUpdatedList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Get Things Done !</h1>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="listOfTask">
+        {todoList.map((todo) => {
+          return (
+            <Task
+              taskName={todo.taskName}
+              id={todo.id}
+              completeStatus={todo.completeStatus}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+              className={`Task${todo.completeStatus ? " completed" : ""}`}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Task.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+export const Task = (props) => {
+  return (
+    <div
+      className={`task ${props.completeStatus ? "completed" : ""}`}
+    //   style={{ backgroundColor: props.completeStatus ? "green" : "white" }}
+    >
+      <h1>{props.taskName}</h1>
+      <button
+        className="delete-button"
+        onClick={() => {
+          props.deleteTask(props.id);
+        }}
+      >
+       
+        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+
+      </button>
+
+      <button
+        className="complete-button"
+        onClick={() => {
+          props.updateTask(props.id);
+        }}
+      >
+        
+         <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
+        </button>
+    </div>
+  );
+};
